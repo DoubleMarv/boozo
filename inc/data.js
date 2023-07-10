f4a3bceebbc96397b51289fac3e2bf61f2f6856d
BarrytronCharlies <info@doublemarvellous.com>
2023-07-10 20:55:56 +0100

    blast off

---
+++ inc/data.js
@@ -0,0 +1,25 @@
+let boozeobject = {
+    "booze": {
+		"rockshore": {
+            "title": "Rockshore Light",
+            "size" :  "330ml",
+            "calories" : "110",
+        },
+        "heineken": {
+            "title": "Heineken",
+            "size" :  "330ml",
+            "calories" : "170",
+        },
+        "guinness": {
+            "title": "Guinness",
+            "size" :  "330ml",
+            "calories" : "210",
+        },
+         "fosters": {
+            "title": "Fosters",
+            "size" :  "330ml",
+            "calories" : "225",
+        },
+
+    }
+}
